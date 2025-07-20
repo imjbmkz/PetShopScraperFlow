8e45d9378b9b81a13a002fe06552b7c9d7ea8f4a
Josh Dev <imjbmkz@gmail.com>
2025-07-21 00:52:46 +0800

    Test Prefect task and flow

--- flows/zooplus_flow.py
+++ flows/zooplus_flow.py
@@ -5,17 +5,35 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 # sys.path.append(str(Path(__file__).parent.parent / "src"))
 
+import datetime as dt
 from prefect import flow, task
 from src.connection import Connection
 from src.factory import SHOPS, run_etl
 
 SHOP_NAME = "Zooplus"
+RUN_DATE = dt.datetime.now().strftime("%Y%m%d")
 client = run_etl(SHOP_NAME)
 
-@task
+@task(
+    name="Get Product URLs",
+    description="Get product URLs (new and old) from the shop.",
+    task_run_name=f"get-{SHOP_NAME}-urls-as-of-{RUN_DATE}"
+)
 def get_product_urls():
-    pass
+    print("Success")
 
-@task
+@task(
+    name="Get Product Details",
+    description="Get product details based on the collected URLs.",
+    task_run_name=f"get-{SHOP_NAME}-product-details-as-of-{RUN_DATE}"
+)
 def get_product_details():
-    pass
+    print("Success")
+
+@flow
+def pipeline():
+    get_product_urls()
+    get_product_details()
+
+if __name__ == "__main__":
+    pipeline()
