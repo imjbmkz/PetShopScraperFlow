28442c22fa3887789e0fe7a55bcdc6186ff48c9e
Ikigami Sama <ikigamidevs.15@gmail.com>
2025-08-14 23:41:46 +0800

    addded bernpetfoods

---
+++ flows/bernpetfoods_get_product_details.py
@@ -0,0 +1,33 @@
+from src.factory import SHOPS, run_etl
+from src.connection import Connection
+from prefect import flow, task
+import datetime as dt
+import sys
+from pathlib import Path
+
+# Allow importing from the src directory
+sys.path.append(str(Path(__file__).parent.parent))
+# sys.path.append(str(Path(__file__).parent.parent / "src"))
+
+
+SHOP_NAME = "BernPetFoods"
+RUN_DATE = dt.datetime.now().strftime("%Y%m%d")
+client = run_etl(SHOP_NAME)
+
+
+@task(
+    name="Get Product Details",
+    description="Get details of each product.",
+    task_run_name=f"get-{SHOP_NAME}-urls-as-of-{RUN_DATE}"
+)
+def get_product_details():
+    client.get_product_infos()
+
+
+@flow
+def pipeline():
+    get_product_details()
+
+
+if __name__ == "__main__":
+    pipeline()
--- src/etl.py
+++ src/etl.py
@@ -25,7 +25,7 @@ class PetProductsETL(ABC):
         self.browser_type = 'chromium'
         self.with_proxy = False
 
-    async def scrape(self, url, selector, proxy=None, headers=None, wait_until="load", min_sec=1, max_sec=3, browser="firefox"):
+    async def scrape(self, url, selector, browser, proxy=None, headers=None, wait_until="load", min_sec=1, max_sec=3):
         soup = await scrape_url(url, selector, proxy, headers, wait_until, min_sec=min_sec, max_sec=max_sec, browser=browser)
         return soup if soup else False
 
